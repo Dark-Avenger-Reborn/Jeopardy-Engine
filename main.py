d453a5e69669cab3a4fdebb4b6f9bc3bf75f7bf3
Oz Abramovich <184321725+Dark-Avenger-Reborn@users.noreply.github.com>
2024-12-17 03:37:34 +0000

    added socket.io server for mutli checking between page support

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
-from flask import Flask, request, jsonify, render_template
-import ansible_automator
-import threading
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import eventlet
 
+# Create the Flask app and initialize SocketIO with eventlet
 app = Flask(__name__)
+socketio = SocketIO(app, async_mode='eventlet')  # Use eventlet for async_mode
 
-@app.route('/',)
+@app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/update_status', methods=['POST'])
-def update_status():
-    # Get the JSON data sent from the client
-    data = request.get_json()
-
+@socketio.on('update_status')
+def handle_update_status(data):
+    """
+    This function is triggered when the 'update_status' event is emitted from the client.
+    It processes the received data and returns a response over the WebSocket.
+    """
     string_of_ips = ""
+    ip_list = []
 
     # Log or process the received data
     print("Received Data:")
     for team in data:
         for ip_state in team['ipStates']:
             if ip_state['isChecked']:
-                print(ip_state['ip'])
                 if (":" in ip_state['ip']):
                     finalIP = ip_state['ip'].split(":")[0]
                 else:
                     finalIP = ip_state['ip']
+                ip_list.append(ip_state['ip'])
                 string_of_ips += finalIP + ","
 
     string_of_ips = string_of_ips[:-1]
     print(string_of_ips)
+    print(ip_list)
 
-
-    inventory_path = "./ansible_recources/real.ini"
-    playbook_path = "/path/to/playbook.yml"
-    
-    #threading.Thread(target=ansible_automator.run_playbook, args=(inventory_path, playbook_path, string_of_ips))
-
-    # Respond with a success message
-    return jsonify({"status": "success", "message": "Data received successfully!"})
+    # Emit a success message back to the client
+    socketio.emit('status_update_all', ip_list)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use eventlet's WSGI server to run the app
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
