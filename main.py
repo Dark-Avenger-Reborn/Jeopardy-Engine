1cab73dcd500908600887d483581cbeec4bd3cdc
Oz Abramovich <184321725+Dark-Avenger-Reborn@users.noreply.github.com>
2025-01-16 14:59:44 -0800

    fixed and formated documentts

--- main.py
+++ main.py
@@ -12,31 +12,10 @@ def main():
 
 @socketio.on('update_status')
 def handle_update_status(data):
-    """
-    This function is triggered when the 'update_status' event is emitted from the client.
-    It processes the received data and returns a response over the WebSocket.
-    """
-    string_of_ips = ""
-    ip_list = []
-
-    # Log or process the received data
-    print("Received Data:")
-    for team in data:
-        for ip_state in team['ipStates']:
-            if ip_state['isChecked']:
-                if (":" in ip_state['ip']):
-                    finalIP = ip_state['ip'].split(":")[0]
-                else:
-                    finalIP = ip_state['ip']
-                ip_list.append(ip_state['ip'])
-                string_of_ips += finalIP + ","
-
-    string_of_ips = string_of_ips[:-1]
-    print(string_of_ips)
-    print(ip_list)
+    print(data)
 
     # Emit a success message back to the client
-    socketio.emit('status_update_all', ip_list)
+    socketio.emit('update_status_all', data)
 
 if __name__ == '__main__':
     # Use eventlet's WSGI server to run the app
